324e20873434911aa76cfea1da7f134b2469dc98
Robin van der Linde <robin.vanderlinde@gmail.com>
2021-05-14 20:17:42 +0200

    ✨ Multiple common words

--- src/index.ts
+++ src/index.ts
@@ -80,7 +80,33 @@ logoutLink.onclick = (ev: Event) => {
 
 // Name Stripper
 function stripName(name: String) {
-  var output = name.replace("laan", "");
+  const commonWords = [
+    new RegExp("^Avenue de la"),
+    new RegExp("^Avenue des"),
+    new RegExp("^Avenue de"),
+    new RegExp("^Avenue d'"),
+    new RegExp("^Avenue"),
+    new RegExp("^Boulevard"),
+    new RegExp("laan$"),
+    new RegExp("lei$"),
+    new RegExp("plein$"),
+    new RegExp("^Rue de la"),
+    new RegExp("^Rue des"),
+    new RegExp("^Rue de"),
+    new RegExp("^Rue du"),
+    new RegExp("^Rue d'"),
+    new RegExp("^Rue"),
+    new RegExp("straat$"),
+    new RegExp("-strasse$", "i"),
+    new RegExp("strasse$", "i"),
+    new RegExp("-straße$", "i"),
+    new RegExp("straße$", "i"),
+    new RegExp("weg$"),
+  ];
+  var output = name;
+  for (var i = 0; i < commonWords.length; i++) {
+    output = output.replace(commonWords[i], "");
+  }
   return output;
 }
 
